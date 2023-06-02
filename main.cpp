8f3b4f6cc63574c691238e6bcf0b93a554422096
artyEZ <abitur.arty@gmail.com>
2023-06-02 20:31:31 +0400

    Implement Frequency beat test

--- main.cpp
+++ main.cpp
@@ -18,13 +18,29 @@ std::vector<bool> generator()
     return number;
 }
 
+void NIST_1(std::vector<bool> input) {
+    double sum = 0;
+    for (auto i : input)
+    {
+        if (i == 0) sum_n += -1;
+        else sum_n += 1;
+    }
+    sum /= sqrt(input.size());
+
+    std::cout << "Frequency beat test: " << erfc(sum / sqrt(2));
+}
 
 int main() {
+
     setlocale(LC_ALL, "Russian");
-    vector<bool> number = generator();
+    std::vector<bool> generated_numbers = generator();
 
     for (const auto& num : generated_numbers)
     {
         std::cout << num << " ";
     }
+
+    std::cout << std::endl;
+
+    NIST_1(generated_numbers);
 }
